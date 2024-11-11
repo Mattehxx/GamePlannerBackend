840f356ce46ab679688c5791973b29583d6d0c55
Matteo Rovellini <teorove04@gmail.com>
2024-11-11 16:35:46 +0100

    Added Email service

--- GamePlanner/Program.cs
+++ GamePlanner/Program.cs
@@ -1,3 +1,5 @@
+using GamePlanner.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,13 +9,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Custom services
+builder.Services.AddTransient<IEmailSender, EmailSender>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
