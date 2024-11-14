67f1e2173603e4db7a48c14e15b7e3ca9722839e
Leonardo Galante <leonardo.galante@itsincom.it>
2024-11-14 14:36:33 +0100

    Add CreateNewRecurrenceSessions in eventManager

--- GamePlanner.DAL/Managers/EventManager.cs
+++ GamePlanner.DAL/Managers/EventManager.cs
@@ -2,16 +2,49 @@
 using GamePlanner.DAL.Data.Entity;
 using GamePlanner.DAL.Managers;
 using GamePlanner.DAL.Managers.IManagers;
+using Microsoft.EntityFrameworkCore;
 
 namespace GamePlanner.Managers
 {
     public class EventManager(GamePlannerDbContext context) : GenericManager<Event>(context), IEventManager
     {
+        public async Task<bool> CreateNewRecurrenceSessions(int eventId, DateTime newDate)
+        {
+            var myEvent = _dbSet.AsQueryable().Include(e => e.Sessions).SingleOrDefault(e => e.EventId.Equals(eventId))
+                ?? throw new InvalidOperationException($"event with id:{eventId} not found");
+            DateTime lastDate = myEvent.Sessions?.Last().StartDate ?? throw new Exception("sessions not found");
+            var sessions = myEvent.Sessions?.Where(s => s.StartDate.Date == lastDate.Date) ?? throw new InvalidOperationException();
+            foreach (var session in sessions)
+            {
+                var start = new DateTime(
+                    newDate.Year,
+                    newDate.Month,
+                    newDate.Day,
+                    session.StartDate.Hour,
+                    session.StartDate.Minute,
+                    session.StartDate.Second
+                );
+                var end = start.AddHours((session.EndDate - session.StartDate).TotalHours);
+                myEvent.Sessions.Add(new Session
+                {
+                    SessionId = 0,
+                    EventId = eventId,
+                    StartDate = start,
+                    EndDate = end,
+                    GameId = session.GameId,
+                    Seats = session.Seats,
+                    IsDeleted = session.IsDeleted,
+                    MasterId = session.MasterId
+                });
+            }
+            return await _context.SaveChangesAsync() > 0;
+        }
+
         public override async Task<Event> DeleteAsync(int id)
         {
             Event entity = await GetByIdAsync(id);
             entity.IsDeleted = true;
-            return await _context.SaveChangesAsync() > 0 
+            return await _context.SaveChangesAsync() > 0
                 ? entity
                 : throw new InvalidOperationException("Failed to delete entity");
         }
