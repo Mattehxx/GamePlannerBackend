4641c506053f04f7351195d42a132f4291532a69
Leonardo Galante <leonardo.galante@itsincom.it>
2024-11-15 10:57:50 +0100

    Add DisableOrEnable in user

--- GamePlanner.DAL/Managers/IManagers/IApplicationUserManager.cs
+++ GamePlanner.DAL/Managers/IManagers/IApplicationUserManager.cs
@@ -1,8 +1,10 @@
 ﻿using GamePlanner.DAL.Data.Auth;
+using GamePlanner.DAL.Data.Entity;
 
 namespace GamePlanner.DAL.Managers.IManagers
 {
     public interface IApplicationUserManager : IManager<ApplicationUser>
     {
+        public Task<ApplicationUser> DisableOrEnableUser(string userId);
     }
 }
