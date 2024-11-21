06ee790d25390432e5a2986054d76cd937248b9e
Luca Gallazzi <luca.gallazzi@itsincom.it>
2024-11-21 13:37:36 +0100

    Added error message to game controller

--- GamePlanner/Controllers/ReservationController.cs
+++ GamePlanner/Controllers/ReservationController.cs
@@ -63,7 +63,7 @@ namespace GamePlanner.Controllers
             }
             catch (Exception ex)
             {
-                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError, new { message   =  ex.Message });
             }
         }
 
